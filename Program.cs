b5cb1dc15a2ce710adfc0bba9f0c04a21531b9ba
Fam. Guardado <Fam. Guardado@LAPTOP-JB7PIS3C>
2023-09-03 14:05:38 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        string usuario = "usuario"; // Cambia esto por el nombre de usuario deseado
+        string contraseña = "contraseña123"; // Cambia esto por la contraseña deseada
+
+        Console.WriteLine("Por favor, ingrese su nombre de usuario:");
+        string usuarioIngresado = Console.ReadLine();
+
+        Console.WriteLine("Por favor, ingrese su contraseña:");
+        string contraseñaIngresada = Console.ReadLine();
+
+        if (usuarioIngresado == usuario && contraseñaIngresada == contraseña)
+        {
+            Console.WriteLine("¡Inicio de sesión exitoso!");
+        }
+        else
+        {
+            Console.WriteLine("Nombre de usuario o contraseña incorrectos. Por favor, inténtelo nuevamente.");
+        }
+
+        Console.WriteLine("Presione cualquier tecla para salir.");
+        Console.ReadKey();
+    }
+}
